03af56d2befc3ee31665698071699cbe7d3039b9
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-07-31 15:02:34 -0300

    Update aula3-2.sql

--- aula3-2.sql
+++ aula3-2.sql
@@ -5,9 +5,9 @@ INSERT INTO travels.users (id, name, email, birth_date, address) VALUES
 (3, 'Pedro Souza', 'pedro@example.com', '1998-02-10', 'Avenue C, 789, City X, State Y');
 
 INSERT INTO travels.destinations (id, name, description) VALUES 
-(1, 'Praia das Tartarugas', 'Uma bela praia com areias brancas e mar cristalino'),
-(2, 'Cachoeira do Vale Verde', 'Uma cachoeira exuberante cercada por natureza'),
-(3, 'Cidade Histórica de Pedra Alta', 'Uma cidade rica em história e arquitetura');
+(1, 'Turtle Beach', 'A beautiful beach with white sand and crystal clear sea'),
+(2, 'Vale Verde Waterfall', 'A lush waterfall surrounded by nature'),
+(3, 'Historic City of Pedra Alta', 'A city rich in history and architecture');
 
 INSERT INTO travels.bookings (id, id_user, id_destination, date, status) VALUES 
 (1, 1, 2, '2023-07-10', 'confirmed'),
@@ -16,16 +16,16 @@ INSERT INTO travels.bookings (id, id_user, id_destination, date, status) VALUES
 
 -- Selects --
 
--- Selecionar todos os registros da tabela "usuarios"
+-- Select all registers from table "users"
 SELECT * FROM users;
 
--- Selecionar apenas o nome e o email dos usuários
+-- Select only name and email from users
 SELECT name, email FROM users;
 
--- Selecionar os usuários que possuem o nome "João Silva"
+-- Select users named "João Silva"
 SELECT * FROM users WHERE name = 'João Silva';
 
--- Selecionar os usuários que nasceram antes de uma determinada data
+-- Select users born before a date
 SELECT * FROM users WHERE birth_date < '1990-01-01';
 
 -- Like
